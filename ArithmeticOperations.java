18c9f7ee620beb4f126b89b76b60e40cfbc77e13
adityachak233-dotcom <adityachak233@gmail.com>
2026-08-02 00:06:30 +0600

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        
+        double a = -5 + 8 * 6;
+        
+        
+        int b = (55 + 9) % 9;
+        
+        
+        double c = 20 + (double) -3 / 8;
+        
+        
+        double d = 5 + (15.0 / 3) * 2;
+        
+        System.out.println("Result of a (-5 + 8 * 6): " + a);
+        System.out.println("Result of b ((55 + 9) % 9): " + b);
+        System.out.println("Result of c (20 + -3 / 8): " + c);
+        System.out.println("Result of d (5 + 15 / 3 * 2): " + d);
+    }
+}
