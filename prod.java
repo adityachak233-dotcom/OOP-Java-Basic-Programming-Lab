e29a4e086e51f80dd33a138d5a07dce1e9eb3d1d
adityachak233-dotcom <adityachak233@gmail.com>
2026-08-02 00:06:55 +0600

    Add files via upload

---
+++ prod.java
@@ -0,0 +1,11 @@
+public class prod  {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 5;
+        int result = num1 * num2;
+        
+        System.out.println(num1 + " * " + num2 + " = " + result);
+    }
+} 
+    
+
